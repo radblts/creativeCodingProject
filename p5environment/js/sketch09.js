b6a5e792bd7c5a788fbd34b4cbecac63164c35bd
radblts <nevaroten@gmail.com>
2025-05-14 09:33:46 +0200

    sketch 09 basic random alive or dead + arrays

--- p5environment/js/sketch09.js
+++ p5environment/js/sketch09.js
@@ -1,7 +1,48 @@
+let cellSize = 15;
+let columns, rows;
+let current = [];
+let next = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
+  frameRate(10);
+
+  columns = floor(width / cellSize);
+  rows = floor(height / cellSize);
+
+  for (let x = 0; x < columns; x++) {
+    current[x] = [];
+    next[x] = [];
+    for (let y = 0; y < rows; y++) {
+      current[x][y] = 0;
+      next[x][y] = 0;
+    }
+  }
+
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      let value = current[x][y];
+      if (value === 1) fill(0);
+      else fill(255);
+      stroke(0);
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function mousePressed() {
+  randomizeBoard();
+  loop();
+}
+
+function randomizeBoard() {
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      current[x][y] = random([0, 1]);
+    }
+  }
 }
